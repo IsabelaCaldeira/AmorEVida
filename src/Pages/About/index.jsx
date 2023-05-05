be41c592e27231a99efea09ba8bed21b5e64419a
brunoholanda <holanda_rodrigues@hotmail.com>
2023-05-05 20:28:32 -0300

    pagina doacoes e ajuste da pagina sobre

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,8 +1,10 @@
 import { about } from "../../constants/db.js";
 import Banner from "../../layouts/Banner";
-import banner from "/assets/img/banner-about.png";
 import "./About.css";
-
+import missao from "/assets/icons/missao.png";
+import valores from "/assets/icons/valores.png";
+import visao from "/assets/icons/visao.png";
+import banner from "/assets/img/banner-about.png";
 export default function About() {
 
   return (
@@ -16,8 +18,9 @@ export default function About() {
 
       <div className="cards">
         <div className="card">
-          <h2>Missão</h2>
           <div className="quadrado">
+            <img src={missao} alt="icone da missao do projeto" />
+            <h2>Missão</h2>
             <p>
               Acolher e capacitar pessoas em situação de vulnerabilidade social, juntamente com auxílio de parcerias,
               restaurando sua dignidade e bem estar social.
@@ -25,8 +28,9 @@ export default function About() {
           </div>
         </div>
         <div className="card">
-          <h2>Visão</h2>
           <div className="quadrado">
+            <img src={visao} alt="icone da visao do projeto" />
+            <h2>Visão</h2>
             <p>
               Ser referência no apoio a pessoas em vulnerabilidade, auxiliando famílias e contribuindo para o
               desenvolvimento social no Norte de Minas Gerais.
@@ -34,8 +38,9 @@ export default function About() {
           </div>
         </div>
         <div className="card">
-          <h2>Valores</h2>
           <div className="quadrado">
+            <img src={valores} alt="icone dos valores do projeto" />
+            <h2>Valores</h2>
             <p>Solidariedade, Disciplina, Respeito e Equidade.</p>
           </div>
         </div>
