6af9c146b520b0e8a721f39430368c959f36cc9e
Matheus <matheus110903@gmail.com>
2023-05-11 18:39:23 -0300

    completed footer

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -9,29 +9,33 @@ import logo from "/assets/img/logo.png";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="content relative max-w-full px-6">
+    <footer className="footer max-sm:mt-12">
+      <div className="content relative max-w-full px-6 max-md:pt-20 ">
+        <div className="md:hidden mx-auto w-fit"><img src={logo} className="max-w-full" alt="" /></div>
+
         <div className="links max-w-full">
           <FooterLinks />
         </div>
 
-        <div className="flex flex-wrap gap-x-2 gap-y-8 mx-auto">
-          <ul className="max-w-lg px-4">
-            <h3 className="text-xl mb-5">Sede administrativa e Casa de acolhimento</h3>
-            <li>Rua Mestre Janjão, 85, São José - Montes Claros/MG, CEP:39400-353 </li>
-            <li>Telefone: +55 (38) 8834-1899</li>
-            {/* <li>Telefone: +55 (38) 98858-5246</li> */}
-            <li>Email: casaamorevida@outlook.com</li>
-            <li>CNPJ: 28.467.302/0001-93</li>
-          </ul>
+        <div className="flex flex-wrap items-center justify-between container gap-x-2 gap-y-8 mx-auto">
+          <div className="xl:flex ">
+            <ul className="max-w-lg mb-8 px-4">
+              <h3 className="text-2xl mb-5">Sede administrativa e Casa de acolhimento</h3>
+              <li>Rua Mestre Janjão, 85, São José - Montes Claros/MG, CEP:39400-353 </li>
+              <li>Telefone: +55 (38) 8834-1899</li>
+              {/* <li>Telefone: +55 (38) 98858-5246</li> */}
+              <li>Email: casaamorevida@outlook.com</li>
+              <li>CNPJ: 28.467.302/0001-93</li>
+            </ul>
 
-          <ul className="max-w-lg px-3">
-            <h3 className="text-xl mb-5">Galpão de reciclagem</h3>
-            <li>Av. Ovídio de Abreu, 419, Centro - Montes Claros/MG, CEP:39400-068</li>
-            <li>Telefone: +55 (38) 98858-5246</li>
-          </ul>
+            <ul className="max-w-md px-3">
+              <h3 className="text-2xl mb-5">Galpão de reciclagem</h3>
+              <li>Av. Ovídio de Abreu, 419, Centro - Montes Claros/MG, CEP:39400-068</li>
+              <li>Telefone: +55 (38) 98858-5246</li>
+            </ul>
+          </div>
 
-          <img src={logo} className="" alt="" />
+          <div className="max-md:hidden"><img src={logo} className="max-w-full" alt="" /></div>
         </div>
       </div>
 
