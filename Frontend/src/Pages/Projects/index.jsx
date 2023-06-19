3674b5827adaba09f968a1e947e2c8152ec3e271
Matheus <matheus110903@gmail.com>
2023-06-19 12:47:40 -0300

    updated imports and text. pages/projects

--- Frontend/src/Pages/Projects/index.jsx
+++ Frontend/src/Pages/Projects/index.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import CollapseMenu from "../../components/CollapseMenu";
-import { posts } from "../../constants/db.js";
+import { posts } from "../../constants/db.jsx";
 import Banner from "../../layouts/Banner";
-import banner from "/assets/img/banner-projects.png";
-import fluxo from "/assets/img/fluxograma.png";
+// import banner from "/assets/img/banner-projects.png";
 import "./Projects.css";
+import banner from "/assets/img/casa.svg";
+import fluxo from "/assets/img/fluxograma.png";
 
 export default function Projects() {
   const [item] = useState(posts[1]);
 
   return (
     <div className="projetos">
-      <Banner img={banner} titulo="NOSSOS PROJETOS" />
+      <Banner img={banner} titulo="A Casa" />
       <div className="post">
         <h2>A Casa de Acolhimento</h2>
         <p>{item.texto}</p>
